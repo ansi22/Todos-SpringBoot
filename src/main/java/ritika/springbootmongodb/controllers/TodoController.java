35edc1c83be44da6bc017e8fe9e4b9c41398fa40
Ritika Verma <iansi@Ritikas-MacBook-Air.local>
2023-09-26 00:04:35 +0530

    added delete by id

--- src/main/java/ritika/springbootmongodb/controllers/TodoController.java
+++ src/main/java/ritika/springbootmongodb/controllers/TodoController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -68,5 +69,16 @@ public class TodoController {
         } else {
             return new ResponseEntity<>("Todo not found with id "+id, HttpStatus.NOT_FOUND);
         }
+    }
+
+    @DeleteMapping("/todos/{id}")
+    public ResponseEntity<?> deleteById(@PathVariable("id") String id) {
+        try {
+        
+        todoRepo.deleteById(id);
+        return new ResponseEntity<> ("Successfully deleted with id "+id, HttpStatus.OK);
+       } catch(Exception e) {
+        return new ResponseEntity<> (e.getMessage(), HttpStatus.NOT_FOUND);
+       }
     }
 }
