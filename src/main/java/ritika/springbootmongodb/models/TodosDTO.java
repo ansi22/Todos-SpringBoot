d921c5269753fe216201f960c218c24cf2db70af
Ritika Verma <iansi@Ritikas-MacBook-Air.local>
2023-09-26 21:26:58 +0530

    added validationconfig

--- src/main/java/ritika/springbootmongodb/models/TodosDTO.java
+++ src/main/java/ritika/springbootmongodb/models/TodosDTO.java
@@ -5,6 +5,7 @@ import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,9 +20,17 @@ import lombok.Setter;
 public class TodosDTO {
     @Id 
     private String id;
+
+    @NotNull(message = "todo cannot be null")
     private String todo;
+
+    @NotNull(message = "description cannot be null")
     private String description;
+
+    @NotNull(message = "completed cannot be null")
     private Boolean completed;
+    
     private Date createdAt;
+    
     private Date updatedAt; 
 }
