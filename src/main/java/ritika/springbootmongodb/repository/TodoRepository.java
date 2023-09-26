5934d14d31ebe230fcae0ccc29b18f3643a34a36
Ritika Verma <iansi@Ritikas-MacBook-Air.local>
2023-09-26 21:49:37 +0530

    added create custom exception

--- src/main/java/ritika/springbootmongodb/repository/TodoRepository.java
+++ src/main/java/ritika/springbootmongodb/repository/TodoRepository.java
@@ -1,6 +1,9 @@
 package ritika.springbootmongodb.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import ritika.springbootmongodb.models.TodosDTO;
@@ -8,4 +11,6 @@ import ritika.springbootmongodb.models.TodosDTO;
 @Repository
 public interface TodoRepository extends MongoRepository<TodosDTO, String> {
     
+    @Query("{'todo': ?0}")
+    Optional<TodosDTO> findByTodo(String todo);
 }
